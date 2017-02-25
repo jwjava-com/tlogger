0a3199801c74579765f3385f0713d2ce584d7891
Jon Warren <jon@jonwarren.info>
2017-02-25 16:49:12 -0600

    updated things long since forgotten about, but not wanting to loose changes

--- src/main/java/info/jonwarren/tlogger/config/ApplicationSetup.java
+++ src/main/java/info/jonwarren/tlogger/config/ApplicationSetup.java
@@ -7,7 +7,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties(prefix = "toggl")
+@ConfigurationProperties(prefix = "dev.toggl")
 public class ApplicationSetup {
 
 	private List<String> keys = new ArrayList<String>();
